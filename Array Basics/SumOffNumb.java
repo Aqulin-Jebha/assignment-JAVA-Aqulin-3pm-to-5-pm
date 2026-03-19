f10b4283e9c0c9d52923e55ea1b0baa2d988120d
Aqulin-Jebha <126853902+Aqulin-Jebha@users.noreply.github.com>
2026-03-19 23:32:23 +0530

    completed

---
+++ Array Basics/SumOffNumb.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SumOffNumb {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+
+        System.out.println("Enter the array size");
+        int length = scan.nextInt();
+
+        int[] array = new int[length];
+
+        System.out.println("Enter the numbers");
+        for(int i = 0; i < length; i++)
+        {
+            array[i] = scan.nextInt();
+        }
+
+        for(int i = 0; i < length; i++)
+        {
+            sum = sum + array[i];
+        }
+
+        System.out.println("Sum of array elements: " + sum);
+    }
+}
